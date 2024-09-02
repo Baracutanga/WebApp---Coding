c57c4aa1d802e7890c031148693c13aacb7d8f59
Baracutanga <lgbr212@gmail.com>
2024-09-01 23:56:35 -0300

    poisé

---
+++ scripts.js
@@ -0,0 +1,57 @@
+const buttonPage = document.getElementById('add-page');
+const nickname = document.getElementById('nick-post').innerText;
+const createPostContainer = document.getElementById('create-post-container');
+const cancelBtn = document.getElementById('cancel-button');
+const confirmBtn = document.getElementById('confirm-button');
+
+// ========================================================
+// EventListener ( Tem mutcho ┐(‘～` )┌ )
+buttonPage.addEventListener('mouseenter', BpEnter)
+buttonPage.addEventListener('mouseleave', function() {
+    buttonPage.style.backgroundColor = ""
+})
+buttonPage.addEventListener('mousedown', BpPressDown)
+buttonPage.addEventListener('mouseup', BpPressUp)
+buttonPage.addEventListener('click', createPostOpen)
+cancelBtn.addEventListener('click', createPostClose)
+confirmBtn.addEventListener('click', enviarPost)
+// ========================================================
+
+// Functions "+" Button
+function BpEnter() {
+    buttonPage.style.backgroundColor = "#e0d2a3"
+}
+
+function BpPressDown() {
+    buttonPage.style.backgroundColor = ""
+}
+
+function BpPressUp() {
+    buttonPage.style.backgroundColor = "#f0e4bc"
+}
+
+// Create Post
+function enviarPost() {
+    let cDiv = document.createElement("div");
+    let newPost = document.createElement('p');
+    let nickPost = document.createElement('h3');
+    let textArea = document.getElementById('post-text');
+
+    cDiv.className = 'posts'
+    nickPost.textContent = nickname;
+    newPost.textContent = textArea.value
+
+    document.getElementById('posts-container').appendChild(cDiv);
+    cDiv.appendChild(nickPost);
+    cDiv.appendChild(newPost);
+
+    createPostClose()
+}
+
+function createPostOpen() {
+    createPostContainer.style.display = "flex"
+}
+
+function createPostClose() {
+    createPostContainer.style.display = ""
+}
